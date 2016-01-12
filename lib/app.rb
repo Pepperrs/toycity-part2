27716c777191f9088b4c862e4ff94f8f467c7f09
Peter Schüllermann <Peter.schuellermann@gmail.com>
2016-01-12 23:27:34 +0100

    add basic structure and asciify

--- lib/app.rb
+++ lib/app.rb
@@ -1,32 +1,31 @@
 require 'json'
 require 'date'
-path = File.join(File.dirname(__FILE__), '../data/products.json')
-file = File.read(path)
-products_hash = JSON.parse(file)
+require 'artii'
+$artii = Artii::Base.new
+def setup
+  path = File.join(File.dirname(__FILE__), '../data/products.json')
+  file = File.read(path)
+  products_hash = JSON.parse(file)
+end
 
 # Print "Sales Report" in ascii art
+def print_in_ascii(text)
+  puts $artii.asciify(text)
+end
 
-# Print today's date
+def print_heading
+  puts
+end
 
-# Print "Products" in ascii art
-puts 'yolo'
-# For each product in the data set:
-# Print the name of the toy
-# Print the retail price of the toy
-# Calculate and print the total number of purchases
-# Calculate and print the total amount of sales
-# Calculate and print the average price the toy sold for
-# Calculate and print the average discount (% or $) based off the average sales price
+def create_report
+  print_heading('salesreport')
+  # print_data
+end
 
-# Print "Brands" in ascii art
-
-# For each brand in the data set:
-# Print the name of the brand
-# Count and print the number of the brand's toys we stock
-# Calculate and print the average price of the brand's toys
-# Calculate and print the total sales volume of all the brand's toys combined
-
-# Print today's date
+def start
+  products_hash = setup
+  create_report(products_hash)
+end
 puts "\nSales Report"
 puts Date.today
 
