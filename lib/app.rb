52f95fc750b49509114cda6b6eccd3db1b2f3283
Peter Schüllermann <Peter.schuellermann@gmail.com>
2016-01-12 23:46:25 +0100

    add print_toy_name, print_toy_price and print_toy_total_purchases

--- lib/app.rb
+++ lib/app.rb
@@ -15,53 +15,53 @@ def print_in_ascii(text)
 end
 
 def print_heading
-  puts
+  print_in_ascii('salesreport')
+  puts Date.today
+end
+
+def print_toy_name(toy)
+  # Print the name of the toy
+  puts "\nName: #{toy['title']}"
+end
+def print_toy_price
+# Print the retail price of the toy
+puts "Retail price: #{toy['full-price']}$"
+end
+def print_toy_total_purchases
+# Calculate and print the total number of purchases
+puts "Total purchases: #{toy['purchases'].length}"
+end
+
+def print_products
+  print_in_ascii('products')
+
+  # For each product in the data set:
+  products_hash['items'].each do |toy|
+    print_toy_name(toy)
+    print_toy_price(toy)
+    print_toy_total_purchases(toy)    # Calculate and print the total amount of sales
+    total_sales = toy['purchases'].map { |purchase| purchase['price'] }.reduce(:+)
+    puts "Revenue total: #{total_sales}$"
+
+    # Calculate and print the average price the toy sold for
+    average_price = total_sales / toy['purchases'].length
+    puts "Average sale price: #{average_price}$"
+
+    # Calculate and print the average discount (% or $) based off the average sales price
+    average_discount_dollar = toy['full-price'].to_f - average_price.to_f
+    average_discount_percent = (100 - average_price.to_f / toy['full-price'].to_f * 100.0)
+    puts "Average discount: #{average_discount_dollar}$ = #{average_discount_percent.round(2)}%"
+  end
 end
 
 def create_report
-  print_heading('salesreport')
-  # print_data
+  print_heading
+  print_products
 end
 
 def start
   setup_files
   create_report($products_hash)
-end
-puts "\nSales Report"
-puts Date.today
-
-puts '                     _            _       '
-puts '                    | |          | |      '
-puts ' _ __  _ __ ___   __| |_   _  ___| |_ ___ '
-puts "| '_ \\| '__/ _ \\ / _` | | | |/ __| __/ __|"
-puts '| |_) | | | (_) | (_| | |_| | (__| |_\\__ \\'
-puts '| .__/|_|  \\___/ \\__,_|\\__,_|\\___|\\__|___/'
-puts '| |                                       '
-puts '|_|                                       '
-
-# For each product in the data set:
-products_hash['items'].each do |toy|
-  # Print the name of the toy
-  puts "\nName: #{toy['title']}"
-
-  # Print the retail price of the toy
-  puts "Retail price: #{toy['full-price']}$"
-
-  # Calculate and print the total number of purchases
-  puts "Total purchases: #{toy['purchases'].length}"
-
-  # Calculate and print the total amount of sales
-  total_sales = toy['purchases'].map { |purchase| purchase['price'] }.reduce(:+)
-  puts "Revenue total: #{total_sales}$"
-
-  # Calculate and print the average price the toy sold for
-  average_price = total_sales / toy['purchases'].length
-  puts "Average sale price: #{average_price}$"
-
-  # Calculate and print the average discount (% or $) based off the average sales price
-  average_discount_dollar = toy['full-price'].to_f - average_price.to_f
-  average_discount_percent = (100 - average_price.to_f / toy['full-price'].to_f * 100.0)
-  puts "Average discount: #{average_discount_dollar}$ = #{average_discount_percent.round(2)}%"
 end
 
 puts ' _                         _     '
